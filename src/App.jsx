4cd11b2e5576e9bb70ea5b7ffa7632037e1c938c
drkpn <drkpn2019@gmail.com>
2025-08-05 23:03:30 +0530

    v5

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import './App.css';
 
 class App extends Component {
   render() {
+    const BASEURL = import.meta.env.BASE_URL;
     return (
       <>
         <header>
@@ -10,27 +11,27 @@ class App extends Component {
         </header>
         <section>
           <div className='card'>
-            <img src={`${import.meta.env.BASE_URL}1.jpeg`} alt='' />
+            <img src={`${BASEURL}1.jpeg`} alt='' />
             <label>Project 01</label>
           </div>
           <div className='card'>
-            <img src={`${import.meta.env.BASE_URL}2.jpeg`} alt='' />
+            <img src={`${BASEURL}2.jpeg`} alt='' />
             <label>Project 02</label>
           </div>
           <div className='card'>
-            <img src={`${import.meta.env.BASE_URL}3.jpeg`} alt='' />
+            <img src={`${BASEURL}3.jpeg`} alt='' />
             <label>Project 03</label>
           </div>
           <div className='card'>
-            <img src={`${import.meta.env.BASE_URL}4.jpeg`} alt='' />
+            <img src={`${BASEURL}4.jpeg`} alt='' />
             <label>Project 04</label>
           </div>
           <div className='card'>
-            <img src={`${import.meta.env.BASE_URL}5.jpeg`} alt='' />
+            <img src={`${BASEURL}5.jpeg`} alt='' />
             <label>Project 05</label>
           </div>
           <div className='card'>
-            <img src={`${import.meta.env.BASE_URL}6.jpeg`} alt='' />
+            <img src={`${BASEURL}6.jpeg`} alt='' />
             <label>Project 06</label>
           </div>
         </section>
